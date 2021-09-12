dd25fbf66d052572cbccba3271b35e10440f7000
argie <argie.gaviola@synerteqsys.com>
2021-09-13 00:06:53 +0800

    updated images

--- src/reward/reward.default.data.ts
+++ src/reward/reward.default.data.ts
@@ -2,19 +2,19 @@ export const REWARD_DEFAULT_DATA = [
     {
         name: "MnM Chocolate",
         description: `They named the candy M&M, which stood for “Mars & Murrie.” ... The deal gave Murrie a 20% stake in the candy, but this stake was later bought out by Mars when chocolate rationing ended at the end of the war in 1948.`,
-        imageUrl: "https://images.unsplash.com/photo-1534705867302-2a41394d2a3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+        imageUrl: "https://images.unsplash.com/photo-1602296751147-5a443571d4a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG0lMjZtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         quantity: 1
     },
     {
         name: "Flight to USA",
         description: `Travel and meet different People`,
-        imageUrl: "https://images.unsplash.com/photo-1628870184564-bea9cd9ccdb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        imageUrl: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmxpZ2h0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         quantity: 100
     },
     {
         name: "1,000 Coins",
         description: `You can buy anything using 1,000 coins`,
-        imageUrl: "https://images.unsplash.com/photo-1554134449-8ad2b1dea29e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        imageUrl: "https://images.unsplash.com/photo-1609726494499-27d3e942456c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGNvaW5zfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         quantity: 100
     },
     {
@@ -26,13 +26,13 @@ export const REWARD_DEFAULT_DATA = [
     {
         name: "XBOX",
         description: `Xbox is a gaming console brand developed and owned by Microsoft. The game console is capable of connecting to a television or other display media. Xbox provides realistic graphics for games. ... Consoles in the Xbox line include Xbox, Xbox 360 and Xbox One.`,
-        imageUrl: "https://images.unsplash.com/photo-1612287230202-1ff1d85d1bdf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+        imageUrl: "https://images.unsplash.com/photo-1622779553291-9f2006fddd5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1125&q=80",
         quantity: 100
     },
     {
         name: "AMD Ryzen 5 2600",
         description: `Ryzen (pronounced RYE zen) is an AMD CPU aimed at the server, desktop, workstation, media center PC and all-in-one markets. AMD's Ryzen base models feature eight cores and 16-thread processing at 3.4Ghz with 20MB cache, neural net-based prediction hardware and smart prefetch.`,
-        imageUrl: "https://images.unsplash.com/photo-1600348712270-5af9e3590f66?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        imageUrl: "https://images.unsplash.com/photo-1591799264318-7e6ef8ddb7ea?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YW1kJTIwcnl6ZW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         quantity: 100
     } 
 ]
