e552dba435075e01c2ba9cd569f69deb09e3022e
argie <argie.gaviola@synerteqsys.com>
2021-09-12 14:46:04 +0800

    updated the mongo url and port in env

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from "dotenv";
 dotenv.config();
-
+console.log("process.env.PORT: ",process.env.PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
