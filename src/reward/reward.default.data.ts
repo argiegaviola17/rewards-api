c51c72cded3152194851bc1630e0793eede48eb5
argie <argie.gaviola@synerteqsys.com>
2021-09-19 03:22:20 +0800

    updated the default data

--- src/reward/reward.default.data.ts
+++ src/reward/reward.default.data.ts
@@ -1,50 +1,96 @@
+// export const REWARD_DEFAULT_DATA = [
+//     {
+//         name: "MnM Chocolate",
+//         description: `They named the candy M&M, which stood for “Mars & Murrie.” ... The deal gave Murrie a 20% stake in the candy, but this stake was later bought out by Mars when chocolate rationing ended at the end of the war in 1948.`,
+//         imageUrl: "https://images.unsplash.com/photo-1602296751147-5a443571d4a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG0lMjZtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//         quantity: 1
+//     },
+//     {
+//         name: "Flight to USA",
+//         description: `Travel and meet different People`,
+//         imageUrl: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmxpZ2h0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//         quantity: 100
+//     },
+//     {
+//         name: "1,000 Coins",
+//         description: `You can buy anything using 1,000 coins`,
+//         imageUrl: "https://images.unsplash.com/photo-1609726494499-27d3e942456c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGNvaW5zfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//         quantity: 100
+//     },
+//     {
+//         name: "PS5",
+//         description: `The PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. ... The PS5, along with Microsoft's Xbox Series X and Series S consoles were released in the same month, is part of the ninth generation of video game consoles.`,
+//         imageUrl: "https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
+//         quantity: 100
+//     },
+//     {
+//         name: "XBOX",
+//         description: `Xbox is a gaming console brand developed and owned by Microsoft. The game console is capable of connecting to a television or other display media. Xbox provides realistic graphics for games. ... Consoles in the Xbox line include Xbox, Xbox 360 and Xbox One.`,
+//         imageUrl: "https://images.unsplash.com/photo-1622779553291-9f2006fddd5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1125&q=80",
+//         quantity: 100
+//     },
+//     {
+//         name: "AMD Ryzen 5 2600",
+//         description: `Ryzen (pronounced RYE zen) is an AMD CPU aimed at the server, desktop, workstation, media center PC and all-in-one markets. AMD's Ryzen base models feature eight cores and 16-thread processing at 3.4Ghz with 20MB cache, neural net-based prediction hardware and smart prefetch.`,
+//         imageUrl: "https://images.unsplash.com/photo-1591799264318-7e6ef8ddb7ea?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YW1kJTIwcnl6ZW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//         quantity: 100
+//     },
+//     {
+//         name: "I Phone",
+//         description: `The iPhone is a smartphone made by Apple that combines a computer, iPod, digital camera and cellular phone into one device with a touchscreen interface.`,
+//         imageUrl: "https://images.unsplash.com/photo-1591815302525-756a9bcc3425?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGlwaG9uZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//         quantity: 100
+//     },
+//     {
+//         name: "Android Phone",
+//         description: `Android is a mobile operating system based on a modified version of the Linux kernel and other open source software, designed primarily for touchscreen mobile devices such as smartphones and tablets. ... Some well known derivatives include Android TV for televisions and Wear OS for wearables, both developed by Google.`,
+//         imageUrl: "https://images.unsplash.com/photo-1514417034809-c7b296354f07?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGFuZHJvaWQlMjBwaG9uZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//         quantity: 100
+//     } 
+// ]
+
 export const REWARD_DEFAULT_DATA = [
     {
-        name: "MnM Chocolate",
-        description: `They named the candy M&M, which stood for “Mars & Murrie.” ... The deal gave Murrie a 20% stake in the candy, but this stake was later bought out by Mars when chocolate rationing ended at the end of the war in 1948.`,
-        imageUrl: "https://images.unsplash.com/photo-1602296751147-5a443571d4a9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG0lMjZtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-        quantity: 1
+            id:"1",
+            name: "Disk",
+            description: `a shape or surface that is round and flat in appearance.`,
+            imageUrl: "https://i.ibb.co/HzKNQrc/1.png",
+            quantity: 100
     },
     {
-        name: "Flight to USA",
-        description: `Travel and meet different People`,
-        imageUrl: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmxpZ2h0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-        quantity: 100
+            id:"2",
+            name: "Cellphone",
+            description: `a phone with access to a cellular radio system so it can be used over a wide area, without a physical connection to a network; a mobile phone.`,
+            imageUrl: "https://i.ibb.co/cYfP3Tr/2.png",
+            quantity: 100
     },
     {
-        name: "1,000 Coins",
-        description: `You can buy anything using 1,000 coins`,
-        imageUrl: "https://images.unsplash.com/photo-1609726494499-27d3e942456c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGNvaW5zfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-        quantity: 100
+            id:"2",
+            name: "Microphone",
+            description: `an instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded.`,
+            imageUrl: "https://i.ibb.co/f4brtH7/3.png",
+            quantity: 100
     },
     {
-        name: "PS5",
-        description: `The PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. ... The PS5, along with Microsoft's Xbox Series X and Series S consoles were released in the same month, is part of the ninth generation of video game consoles.`,
-        imageUrl: "https://images.unsplash.com/photo-1606144042614-b2417e99c4e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
-        quantity: 100
+            id:"2",
+            name: "Car",
+            description: `a four-wheeled road vehicle that is powered by an engine and is able to carry a small number of people.`,
+            imageUrl: "https://i.ibb.co/qkmqqP5/4.png",
+            quantity: 100
     },
     {
-        name: "XBOX",
-        description: `Xbox is a gaming console brand developed and owned by Microsoft. The game console is capable of connecting to a television or other display media. Xbox provides realistic graphics for games. ... Consoles in the Xbox line include Xbox, Xbox 360 and Xbox One.`,
-        imageUrl: "https://images.unsplash.com/photo-1622779553291-9f2006fddd5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1125&q=80",
-        quantity: 100
+            id:"2",
+            name: "Blue",
+            description: `of a color intermediate between green and violet, as of the sky or sea on a sunny day.
+            "the clear blue sky`,
+            imageUrl: "https://i.ibb.co/7nCb14W/5.png",
+            quantity: 100
     },
     {
-        name: "AMD Ryzen 5 2600",
-        description: `Ryzen (pronounced RYE zen) is an AMD CPU aimed at the server, desktop, workstation, media center PC and all-in-one markets. AMD's Ryzen base models feature eight cores and 16-thread processing at 3.4Ghz with 20MB cache, neural net-based prediction hardware and smart prefetch.`,
-        imageUrl: "https://images.unsplash.com/photo-1591799264318-7e6ef8ddb7ea?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YW1kJTIwcnl6ZW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-        quantity: 100
-    },
-    {
-        name: "I Phone",
-        description: `The iPhone is a smartphone made by Apple that combines a computer, iPod, digital camera and cellular phone into one device with a touchscreen interface.`,
-        imageUrl: "https://images.unsplash.com/photo-1591815302525-756a9bcc3425?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGlwaG9uZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-        quantity: 100
-    },
-    {
-        name: "Android Phone",
-        description: `Android is a mobile operating system based on a modified version of the Linux kernel and other open source software, designed primarily for touchscreen mobile devices such as smartphones and tablets. ... Some well known derivatives include Android TV for televisions and Wear OS for wearables, both developed by Google.`,
-        imageUrl: "https://images.unsplash.com/photo-1514417034809-c7b296354f07?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGFuZHJvaWQlMjBwaG9uZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-        quantity: 100
-    } 
-]
+            id:"2",
+            name: "PS5",
+            description: `The PlayStation 5 (PS5) is a home video game console developed by Sony Interactive Entertainment. ... The PS5, along with Microsoft's Xbox Series X and Series S consoles were released in the same month, is part of the ninth generation of video game consoles.`,
+            imageUrl: "https://i.ibb.co/yqcZMtk/6.png",
+            quantity: 100
+    }
+  ]
